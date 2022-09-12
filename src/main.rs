ce8b205ba4b60775c4492ee5db30d5b2c0548ed0
marziply <contact@marziply.dev>
2022-09-12 09:07:12 +0100

    Implemented command structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct App {
+  #[clap(subcommand)]
+  command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Sender {
+  #[clap(subcommand)]
+  r#Send(Data),
+}
+
+#[derive(Debug, Subcommand)]
+enum Data {
+  r#String { data: String },
+  Number { data: i32 },
+  File { path: PathBuf },
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+  Client {
+    #[clap(subcommand)]
+    data: Sender,
+  },
+  Server,
+}
+
 fn main() {
-  println!("Hello, world!");
+  let app = App::parse();
+
+  println!("{app:#?}");
 }
