850f52a8bfcb95d24bda1973331fd3570dbeddc0
marziply <contact@marziply.dev>
2022-09-12 09:53:49 +0100

    Added receive_data server call

---
+++ build.rs
@@ -0,0 +1,8 @@
+use std::error::Error;
+use tonic_build::compile_protos;
+
+fn main() -> Result<(), Box<dyn Error>> {
+  compile_protos("proto/service.proto")?;
+
+  Ok(())
+}
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,26 @@
-use tonic::transport::Server;
+use tonic::{transport::Server, Request, Response, Status};
+
+use self::service::{service_server::Service, DataRequest, DataResponse};
+
+pub mod service {
+  tonic::include_proto!("emotech_service");
+}
+
+struct DataService;
+
+#[tonic::async_trait]
+impl Service for DataService {
+  async fn receive_data(
+    &self,
+    request: Request<DataRequest>,
+  ) -> Result<Response<DataResponse>, Status> {
+    println!("Received request");
+
+    let res = DataResponse { ok: true };
+
+    Ok(Response::new(res))
+  }
+}
 
 pub async fn start_server() {
   let _ = Server::builder();
